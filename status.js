8c94fdb95d06f3b338cd700ae08f58ae0616064d
emyethok <54607965+emyethok@users.noreply.github.com>
2022-12-30 14:06:02 +0700

    Update status.js

--- status.js
+++ status.js
@@ -4,7 +4,7 @@ e.height=n}}}var cstrload=function(){var e=document.getElementById("cstrlb"),t=d
 e&&"undefined"!=e&&"null"!=e&&void 0!=e&&null!=e&&t&&"undefined"!=t&&"null"!=t&&void 0!=t&&null!=t||alert("Please do not remove the . \n IT IS GOING TO BE HIDDEN ANYWAY.")
 var n=e.firstChild.nodeValue,a=e.href,r=t.firstChild.nodeValue,d=t.href
 if("Radio Online"!=n||""!=a||"Radio Broadcaster"!=r||""!=d)alert("Please do not remove the  link.\n  IT IS GOING TO BE HIDDEN ANYWAY.")
-else{e.style.display="true",t.style.display="true"
+else{e.style.display="none",t.style.display="none"
 var s="//www.caster.fm/widgets/em_status.php?uid="+cstruid+"&dark="+cstrdark,i=document.createElement("iframe")
 i.frameBorder=0,i.width="300px",i.id="cstrframe",i.scrolling="no",i.allowtransparency="true",i.setAttribute("src",s),i.setAttribute("onload","cstrsizeFrame();"),i.setAttribute("allowtransparency","true"),document.getElementById("cstrdiv").appendChild(i)}}
 bindEvent(window,"load",cstrload)
